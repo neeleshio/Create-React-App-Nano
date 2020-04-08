2740e34d39602d91f481865551aea7bac21d7b72
Neelesh Shetty <an.neelesh@novasemita.org>
2020-04-08 12:25:38 +0530

    create react app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { Component } from "react";
+import './App.css'
 
-export default () => (
-  <>
-    <h1>Welcome to React Parcel Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
-  </>
-);
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>React</h1>
+        <p>A JavaScript library for building user interfaces</p>
+        <button type='button' ><a href="https://reactjs.org/docs/getting-started.html">Get Started</a></button>
+      </div>
+    )
+  }
+}
+
+export default App;
